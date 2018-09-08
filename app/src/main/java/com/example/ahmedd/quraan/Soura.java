b5deee05f1d54370ca6dab793bddd7ec3f378036
Ahmed <ahmedmohamedaneng@gmail.com>
2018-09-09 01:00:50 +0200

    view_linebyLine_added

--- app/src/main/java/com/example/ahmedd/quraan/Soura.java
+++ app/src/main/java/com/example/ahmedd/quraan/Soura.java
@@ -1,18 +1,32 @@
 package com.example.ahmedd.quraan;
 
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 public class Soura extends AppCompatActivity {
 
     TextView txtSoura;
 
     public static String txtFile = "";
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +36,37 @@ public class Soura extends AppCompatActivity {
         txtSoura.setMovementMethod(new ScrollingMovementMethod());
 
 
-
-        String text = "";
+        StringBuilder returnString = new StringBuilder();
+        InputStream fIn = null;
+        InputStreamReader isr = null;
+        BufferedReader input = null;
         try {
-            InputStream inputStream = getAssets().open(txtFile);
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            text = new String(buffer);
-        } catch (IOException e) {
-            e.printStackTrace();
+            Context context = this;
+            fIn = context.getResources().getAssets()
+                    .open(txtFile, Context.MODE_WORLD_READABLE);
+            isr = new InputStreamReader(fIn);
+            input = new BufferedReader(isr);
+            String line = "";
+            int i = 1;
+            while ((line = input.readLine()) != null) {
+                txtSoura.append(line + "(" + i + ")" + "\n" );
+                i++;
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        } finally {
+            try {
+                if (isr != null)
+                    isr.close();
+                if (fIn != null)
+                    fIn.close();
+                if (input != null)
+                    input.close();
+            } catch (Exception e2) {
+                e2.getMessage();
+            }
         }
-        txtSoura.setText(text);
+
+
     }
 }

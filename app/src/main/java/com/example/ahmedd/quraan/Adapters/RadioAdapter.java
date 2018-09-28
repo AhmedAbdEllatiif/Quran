af869f12557c915186354644159677b1208cf0b5
Ahmed <ahmedmohamedaneng@gmail.com>
2018-09-28 22:20:57 +0200

    RadioAdapter

--- app/src/main/java/com/example/ahmedd/quraan/Adapters/RadioAdapter.java
+++ app/src/main/java/com/example/ahmedd/quraan/Adapters/RadioAdapter.java
@@ -1,4 +1,34 @@
 package com.example.ahmedd.quraan.Adapters;
 
-public class RadioAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RadioAdapter extends RecyclerView.Adapter<RadioAdapter.ViewHolder> {
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
 }
